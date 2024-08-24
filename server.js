5aab7108db9c4ed19b4ddb5b2ab1dcf1424eefe1
thenethi <aravindnethi1632@gmail.com>
2024-08-24 12:49:59 +0530

    updated with body parser

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // Database setup
 const db = new sqlite3.Database("./blog.sqlite", (err) => {
